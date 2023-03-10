789a5a9c58e9963e3caff191741e6fc54f16d847
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-03-10 19:28:17 +0100

    Upgrade animation on box

--- js/script.js
+++ js/script.js
@@ -6,11 +6,7 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 
 
-/*
-Development of the "for loop", replacing multiples of 3 and 5 
-with FizzBuzz, multiples of 3 with Fizz 
-and multiples of 5 with Buzz.
-*/  
+ //Assign the variables
 let c92BoxBoard = document.querySelector('div.c92BoxBoard');
 
 let myInput = document.getElementById('myInput');
@@ -20,53 +16,69 @@ let btnGenerate = document.getElementById('btnGenerate');
 
 
 
-    btnGenerate.addEventListener('click', function(){
+//Start function for btnGenerate when click
+btnGenerate.addEventListener('click', function(){
 
-        if(myInput.value <= 0 || myInput.value > 100){
-            let box = `<div class="m-3 d-flex justify-content-center align-items-center">"Invalid data enter: min 1 and max 100, please try again"</div>`;
+    if(myInput.value <= 0 || myInput.value > 100){
+        let box = `<div class="m-3 d-flex justify-content-center align-items-center">"Invalid data enter: min 1 and max 100, please try again"</div>`;
             c92BoxBoard.innerHTML = box;
-            return;
-        }
+        return;
+    }
     
-        
+    //clear before function start
+    c92BoxBoard.innerHTML = '';
 
-        c92BoxBoard.innerHTML = '';
-        for(let i = 1; i <= myInput.value; i++){
+    /*
+    Development of the "for loop", replacing multiples of 3 and 5 
+    with FizzBuzz, multiples of 3 with Fizz 
+    and multiples of 5 with Buzz.
+    */ 
+    for(let i = 1; i <= myInput.value; i++){
         
-            let stringV = '';
-            let stringBg = '';
-        
-            switch(true){
-                case((i % 3 == 0 ) && (i % 5 == 0)):
+        let stringV = '';
+        let stringBg = '';
+
+        //Start switch for control
+        switch(true){
+            case((i % 3 == 0 ) && (i % 5 == 0)):
                 stringV = 'FizzBuzz';
                 stringBg = 'c92FizzBuzz';
-                    console.log('FizzBuzz');
-                break;
+                console.log('FizzBuzz');
+            break;
         
-                case(i % 3 == 0):
-                    stringV = 'Fizz';
-                    stringBg = 'c92Fizz';
-                    console.log('Fizz');
-                break;
+            case(i % 3 == 0):
+                stringV = 'Fizz';
+                stringBg = 'c92Fizz';
+                console.log('Fizz');
+            break;
         
-                case(i % 5 == 0):
-                    stringV = 'Buzz';
-                    stringBg = 'c92Buzz';
-                    console.log('Buzz');
-                break;
+            case(i % 5 == 0):
+                stringV = 'Buzz';
+                stringBg = 'c92Buzz';
+                console.log('Buzz');
+            break;
         
-                default:
-                    stringV = i;
-                    stringBg = 'c92Default';
-                    console.log(i);
-                break;
-        
-            }
-            let box = `<div class="c92Box m-3 d-flex justify-content-center align-items-center ${stringBg}">${stringV}</div>`;
-            c92BoxBoard.innerHTML += box;
+            default:
+                stringV = i;
+                stringBg = 'c92Default';
+                console.log(i);
+            break;
         
         }
-    });
+
+        /*
+        Create let box and added in html with myInput.value,
+        added variable id for all box generated for added animation
+        */
+        let box = `<div id="boxA-${i}" class="c92Box m-3 d-flex justify-content-center align-items-center ${stringBg}">${stringV}</div>`;
+        c92BoxBoard.innerHTML += box;
+        let animationBox = document.getElementById(`boxA-${i}`);
+        animationBox.classList += ' c92Animation'; 
+            
+        animationBox.style.animationDuration = `${(i * (120 - myInput.value))}ms`;
+        
+    }
+});
 
 
 
