c3e036f75d69a8c8c670426e7752ffa68088f8b9
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-03-10 17:00:47 +0100

    Upgrade html, css,  js

--- js/script.js
+++ js/script.js
@@ -13,30 +13,41 @@ and multiples of 5 with Buzz.
 */  
 let c92BoxBoard = document.querySelector('div.c92BoxBoard');
 
-for(let i = 1; i <= 100; i++){
 
-    let box = `<div class="c92Box debug">${i}</div>`;
-    c92BoxBoard.innerHTML += box;
+for(let i = 1; i <= 100; i++){
+    
+    let stringV = '';
+    let stringBg = '';
+
     switch(true){
         case((i % 3 == 0 ) && (i % 5 == 0)):
-        box.innerHTML += 'FizzBuzz';
+        stringV = 'FizzBuzz';
+        stringBg = 'c92FizzBuzz';
             console.log('FizzBuzz');
         break;
 
         case(i % 3 == 0):
+            stringV = 'Fizz';
+            stringBg = 'c92Fizz';
             console.log('Fizz');
         break;
 
         case(i % 5 == 0):
+            stringV = 'Buzz';
+            stringBg = 'c92Buzz';
             console.log('Buzz');
         break;
 
         default:
+            stringV = i;
+            stringBg = 'c92Default';
             console.log(i);
         break;
 
     }
-    
+    let box = `<div class="c92Box m-3 d-flex justify-content-center align-items-center ${stringBg}">${stringV}</div>`;
+    c92BoxBoard.innerHTML += box;
+
 }
 
 
